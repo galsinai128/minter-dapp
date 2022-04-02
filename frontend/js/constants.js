31fa85a0eca012ebb206350103587aad39a166f5
gal sinai <galsinay128@gmail.com>
2022-04-02 10:13:03 +0300

    test deploy

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x855631DfA7837AD3Bb209E277853b3677FA2159a"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
